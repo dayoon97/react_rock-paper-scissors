fa22283add517c423971d3c70aa8d04eb8684aa6
dayoon97 <ekdbsekdbs@gmail.com>
2020-12-10 15:56:59 +0900

    문서 변경 : 이미지 경로 추가
    사용자가 골라 띄울 이미지 태그 추가

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import jQuery from "jquery";
 import { render } from '@testing-library/react';
 window.$ = window.jQuery = jQuery;
 
-<Control onChangeMode={function (_mode) {
-  this.setState({ mode : _mode});
-}.bind(this)}></Control>
 
 class App extends React.Component {
   render() {
@@ -19,19 +16,18 @@ class App extends React.Component {
         <div className="App">
               <div className="lay1"></div>
               <div className="lay2">
-                  <div></div>
+                {/* <img src={rock} className="rockImg"></img> */}
+                {/* <img src={scissors} className="sciImg"></img> */}
+                <img src={paper} className="paImg"></img>
               </div>
               <div className="lay3"></div>
               <div className="lay4">
-                <div className="rock-area"><img className="rock-size" id="rock" src = {rock} onClick={function(e){
-                  e.preventDefault();
-                  this.props.onChangeMode('rock');
-                }}></img></div>
+                <div className="rock-area"><img className="rock-size" id="rock" src = {rock}></img></div>
                 <div className="scissors-area"><img className="scissors-size" id="scissors" src = {scissors}></img></div>
                 <div className="paper-area"><img className="paper-size" id="paper" src= {paper}></img></div>
               </div>
         </div>
-    </Fragment>
+      </Fragment>
     );
   }
   
