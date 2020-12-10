f510edee255e8df8e8d2585d1489a0c958b1787b
dayoon97 <ekdbsekdbs@gmail.com>
2020-12-10 14:36:16 +0900

    함수 추가
    function -> class 로 변경
    rock을 누르면 lay2에 rock이미지가 뜨게 시도중

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 import rock from './rock.jpg';
 import scissors from './scissors.jpg';
 import paper from './paper.jpg';
 import jQuery from "jquery";
+import { render } from '@testing-library/react';
 window.$ = window.jQuery = jQuery;
 
-function App() {
-  return (
-    <div className="App">
-        <div className="lay1"></div>
-        <div className="lay2"></div>
-        <div className="lay3"></div>
-        <div className="lay4">
-          <div className="rock-area"><img className="rock-size" id="rock" src = {rock}></img></div>
-          <div className="scissors-area"><img className="scissors-size" id="scissors" src = {scissors}></img></div>
-          <div className="paper-area"><img className="paper-size" id="paper" src= {paper}></img></div>
+function rock(props){
+  return <img src={rock}></img>;
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="App">
+              <div className="lay1"></div>
+              <div className="lay2">
+                  
+              </div>
+              <div className="lay3"></div>
+              <div className="lay4">
+                <div className="rock-area"><img className="rock-size" id="rock" src = {rock} onClick={() => rock()}></img></div>
+                <div className="scissors-area"><img className="scissors-size" id="scissors" src = {scissors}></img></div>
+                <div className="paper-area"><img className="paper-size" id="paper" src= {paper}></img></div>
+              </div>
         </div>
-    </div>
-  );
+    </Fragment>
+    );
+  }
+  
 }
-
-
-function hover() {
-    (document).on('click', '#rock', function(event){
-        console.log("ㅇㅇ");
-      
-    });
-}
-
 export default App;
+
+
+
+
+
